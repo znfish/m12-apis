1d0a9fd2be1d60f598319533ec6c8dcd82a28aee
Michael Freeman <michael.k.freeman@gmail.com>
2017-04-20 10:12:13 -0700

    Updated ex4 and ex5 to httr

--- exercise-5/exercise.R
+++ exercise-5/exercise.R
@@ -1,14 +1,15 @@
 ### Exercise 5 ###
 library(jsonlite)
 library(dplyr)
+library(httr)
 
-# Write a function that allows you to specify a movie, then does the following:
-  
-  # Replace all of the spaces in your movie title with plus signs (+)
-  
+# Write a function that allows you to specify a movie, that does the following:
+GetReview <- function(movie) {
   
   # Construct a search query using YOUR api key
   # The base URL is https://api.nytimes.com/svc/movies/v2/reviews/search.json?
+  # Your parameters should include a "query" and an "api_key"
+  # Note: the HTTR library will take care of spaces in the arguments
   # See the interactive console for more detail:https://developer.nytimes.com/movie_reviews_v2.json#/Console/GET/reviews/search.json
   
   
@@ -22,9 +23,10 @@ library(dplyr)
   
   # From the most recent review, store the headline, short summary, and link to full article each in their own variables
   
-  
+
   # Return an list of the three pieces of information from above
   
+}
 
 # Test that your function works with a movie of your choice
 
